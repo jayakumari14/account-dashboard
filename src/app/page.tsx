af079ad41c3e0f14bbd1c66f556b94eadf7792dd
jayak <kumarijaya127@gmail.com>
2024-11-25 01:12:25 +0530

    app directory removed from src folder.

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>hello desktop dashboard</div>;
-};
-
-export default page;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+// src/pages/index.tsx
+import React from "react";
+
+const HomePage = () => {
+  return <div>Welcome to the Dashboard!</div>;
+};
+
+export default HomePage;
