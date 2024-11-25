44aa726d23e1a73b20382645b6bac6849f36d862
jayak <kumarijaya127@gmail.com>
2024-11-26 00:05:55 +0530

    ledger get api functionality added

--- src/pages/api/expenses/index.ts
+++ src/pages/api/expenses/index.ts
@@ -30,5 +30,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   }
 
+  if (req.method === 'GET') {
+    try {
+      // Fetch expenses from the database
+      const expenses = await prisma.expense.findMany();
+      res.status(200).json(expenses);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch expenses' });
+      console.log(error);
+      
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
   return res.status(405).json({ message: "Method not allowed." });
 }
